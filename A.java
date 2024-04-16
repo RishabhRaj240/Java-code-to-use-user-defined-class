d3459417e9b2c2592b5b41be661c17bcabd5c33b
Rishabh Raj <rishabhraj240405@gmail.com>
2024-04-16 10:41:24 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,13 @@
+class A {
+    void m1() {
+      System.out.println("m1");      
+    }
+
+    void m2() {
+      System.out.println("m2");      
+    }
+
+    public static void main(String[] args) {
+      System.out.println("main");      
+    }
+}
